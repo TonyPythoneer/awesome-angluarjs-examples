f6f15c38d61e34c8bd98fd59a325ee9feaec0cbb
TonyPythoneer <tonypythoneer@gmail.com>
2015-10-12 17:27:34 +0800

    Enhance: Add calculator-action.js

--- calculator/v2/calculator-action.js
+++ calculator/v2/calculator-action.js
@@ -3,36 +3,67 @@ angular
   .controller('calculatorPanel', [function() {
     // Local variables
     var self = this;
-    var memory = 0;
+    var memory = '0';
     var initiation = true;
 
-    self.monitor = 0;
-    self.calculatorButtons = [[7, 8, 9, '/'],
-                              [4, 5, 6, 'x'],
-                              [1, 2, 3, '-'],
-                              [0, '.', '=', '+', 'c']];
+    self.monitor = '0';
+    self.calculatorButtons = [['7', '8', '9', '/'],
+                              ['4', '5', '6', 'x'],
+                              ['1', '2', '3', '-'],
+                              ['0', '.', '=', '+', 'c']];
 
     // Define function
     var inputNumber = function(init, num){
-      if (init){
-        self.monitor = self.monitor * 10 + num;
+      if (self.monitor != '0' || num != '0'){
+        if (init){
+          self.monitor = num;
+          initiation = false;
+        }else{
+          self.monitor = self.monitor + num;
+        }
+      }
+    }
+
+    var inputOperation = function(op){
+      if (self.operation){
+        memory = eval(memory + self.operation + self.monitor)
+        self.monitor = memory;
       }else{
-        self.monitor = num;
-        initiation = false;
+        memory = self.monitor
+      }
+      self.operation = op;
+      initiation = true;
+    }
+
+    var inputPoint = function(){
+      self.monitor = self.monitor.toString();
+      indexPoint = self.monitor.indexOf('.');
+      if (!!~indexPoint){
+        if (indexPoint == self.monitor.length-1){
+          self.monitor = self.monitor.replace('.', '');
+        }
+      }else{
+        self.monitor = self.monitor + '.';
       }
     }
 
     var inputClear = function(){
-      self.monitor = 0;
+      self.monitor = '0';
       initiation = true;
     }
 
     self.clickButton = function(input){
-      if (parseInt(input) != NaN){
+      if (!isNaN(parseInt(input))){
         inputNumber(initiation, input);
       }else{
-        //inputOperation()
-        //inputClear()
+        if (!!~['+', '-', '*', '/'].indexOf(input)){
+          inputOperation(input);
+        }else if (input=='.'){
+          inputPoint();
+        }else if (input=='c'){
+          inputClear();
+        }
       }
+      console.log(initiation);
     }
 }]);
