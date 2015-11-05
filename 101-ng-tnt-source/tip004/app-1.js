a01a4511d6ec3ea0b351ac61911323304bf7b7b4
TonyPythoneer <tonypythoneer@gmail.com>
2015-11-05 11:05:54 +0800

    Enhance: Finish app-1 of tip004

--- 101-ng-tnt-source/tip004/app-1.js
+++ 101-ng-tnt-source/tip004/app-1.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('tip004.module', [])
+        .module('tip004', [])
 })();
 
 
@@ -10,15 +10,15 @@
     'use strict';
 
     angular
-        .module('tip004.module')
+        .module('tip004')
         .controller('tip004Controller', tip004Controller)
 
     tip004Controller.$injector = ['$log'];
 
     function tip004Controller($log){
         var self = this;
-        var num = 0
-        self.range = range;
+        var num = 0;
+        self.fields = range(0,2);
         self.sendInputField = sendInputField;
         self.clickTheField = clickTheField;
 
@@ -29,18 +29,32 @@
 
             var list_ = [];
 
-            for (var i = first; i < last; i+=step) list_.push(i);
+            for (var i = first; i < last; i+=step) list_.push({num: 0, val: '', checked: false});
 
             return list_;
         }
 
         function clickTheField(index){
-            $log.log("message is sent to subscriber # %s", index)
+            if (self.fields[index]['checked']){
+                self.fields[index]['checked'] = false;
+                $log.warn("subscriber # %s is removed", self.fields[index]['num'])
+            }else{
+                num++;
+                self.fields[index]['num'] = num;
+                self.fields[index]['checked'] = true;
+                $log.log("message is sent to subscriber # %s", num)
+            }
         }
 
         function sendInputField(){
             $log.log("message is sent to subscriber # %s", 0)
             self.output = self.input;
+            for (var i = 0 ; i < self.fields.length; i++) {
+                if (self.fields[i]['checked']) {
+                    self.fields[i]['val'] = self.input;
+                    $log.log("message is sent to subscriber # %s", self.fields[i]['num'])
+                };
+            };
         }
 
     }
