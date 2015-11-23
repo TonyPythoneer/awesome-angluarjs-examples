4fc1719818da51ba98368767061f9d4b83d8fb24
TonyPythoneer <tonypythoneer@gmail.com>
2015-11-23 15:59:06 +0800

    Enhance: Add ng-class for github-apis

--- interact-with-restful/github-apis/github-app.controller.js
+++ interact-with-restful/github-apis/github-app.controller.js
@@ -14,8 +14,9 @@
         self.enablePrevBtn = false;
         self.enableNextBtn = true;
         self.currentPage = 1;
-        self.clickPreviousOrNextBtn = clickPreviousOrNextBtn;
+        self.clickPrevOrNextBtn = clickPrevOrNextBtn;
         self.showTheItemsInRange = showTheItemsInRange;
+        self.enablePrevOrNextBtn = enablePrevOrNextBtn;
 
         self.myRepoes = GithubApisFactory.query({username: self.author}, function(res, headers){
             var _headers = headers();
@@ -27,19 +28,30 @@
 
         ///
 
-        function clickPreviousOrNextBtn(paging){
+        function clickPrevOrNextBtn(paging){
             var allowPageDown = paging === -1 && self.currentPage !== 1;
             var allowPageUp = paging === 1 && self.currentPage !== maxPage;
             //$log.debug(allowPageDown, allowPageUp);
             if (allowPageDown || allowPageUp){
                 self.currentPage += paging;
-                self.enablePrevBtn = (self.clickPreviousOrNextBtn === 1)? false: true;
-                self.enableNextBtn = (self.clickPreviousOrNextBtn === maxPage)? false: true;
+                self.enablePrevBtn = (self.currentPage === 1)? false: true;
+                self.enableNextBtn = (self.currentPage === maxPage)? false: true;
             }
         }
 
         function showTheItemsInRange(page, index){
             return (page-1)*10 <= index  && index < page*10
+        }
+
+        function enablePrevOrNextBtn(nameBtn){
+            if (nameBtn === 'prev'){
+                $log.debug({disabled: !self.enablePrevBtn});
+                return {disabled: !self.enablePrevBtn}
+            }
+            if (nameBtn === 'next'){
+                $log.debug({disabled: !self.enableNextBtn});
+                return {disabled: !self.enableNextBtn}
+            }
         }
 
     }
